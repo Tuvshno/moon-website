c92ba1381b90344b970f47907ef9248901b4dfa5
tuvshno <tuvshno@gmail.com>
2022-08-18 18:23:17 -0400

    destination page

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const navToggle = document.querySelector(".mobile-nav-toggle");
 
 navToggle.addEventListener("click", () => {
   const visibility = nav.getAttribute("data-visible");
-  if(visibility === "false") {
+  if (visibility === "false") {
     nav.setAttribute("data-visible", "true");
     navToggle.setAttribute("aria-expanded", "true");
   }
@@ -17,5 +17,5 @@ navToggle.addEventListener("click", () => {
 
   console.log(visibility);
 
-  
+
 })
